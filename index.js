c15563d21fd33b4e03ff9c82c1aa54f81eef8d96
renato-albuquerque <renatomalb83@gmail.com>
2023-02-10 23:58:25 -0300

    added share button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const buttonDarkMode = document.querySelector('.darkMode')
 const buttonLightMode = document.querySelector('.lightMode')
+const buttonShare = document.querySelector('.share')
 const html = document.querySelector('html')
 
 
@@ -11,10 +12,12 @@ buttonLightMode.addEventListener('click', function() {
     html.classList.add('lightMode')
     buttonLightMode.classList.add('iconTurnsColor')
     buttonDarkMode.classList.add('iconTurnsColor')
+    buttonShare.classList.add('iconTurnsColor')
 })
 
 buttonDarkMode.addEventListener('click', function() {
     html.classList.remove('lightMode')
     buttonLightMode.classList.remove('iconTurnsColor')
     buttonDarkMode.classList.remove('iconTurnsColor')
+    buttonShare.classList.remove('iconTurnsColor')
 })
