a0453dc6966a90eb95eec8b9bd4f49b13048a2fd
Renato Albuquerque <renatomalb83@gmail.com>
2023-07-24 22:02:43 -0300

    refactor: updated index.html and index.js files

--- index.js
+++ index.js
@@ -5,21 +5,14 @@ const buttonLightMode = document.querySelector('.lightMode')
 const buttonShare = document.querySelector('.share')
 const html = document.querySelector('html')
 
-
 // EVENTS
 
 buttonLightMode.addEventListener('click', function() {
     html.classList.add('lightMode')
-    buttonLightMode.classList.add('iconTurnsColor')
-    buttonDarkMode.classList.add('iconTurnsColor')
-    buttonShare.classList.add('iconTurnsColor')
 })
 
 buttonDarkMode.addEventListener('click', function() {
     html.classList.remove('lightMode')
-    buttonLightMode.classList.remove('iconTurnsColor')
-    buttonDarkMode.classList.remove('iconTurnsColor')
-    buttonShare.classList.remove('iconTurnsColor')
 })
 
 // FUNCTION
